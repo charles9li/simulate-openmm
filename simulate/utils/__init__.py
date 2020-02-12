a6fb1345ca2df124c384b7a3ec17512d9fa60f7e
Charles Li <charles9000li@gmail.com>
2020-02-12 15:18:38 -0800

    created LineDeque class

---
+++ simulate/utils/__init__.py
@@ -0,0 +1,1 @@
+from .line_deque import LineDeque
---
+++ simulate/utils/line_deque.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+
+class LineDeque(object):
+    """ Wrapper around collections.deque object that only adds a line to the
+    deque if it's not empty.
+
+    """
+
+    def __init__(self):
+        self._deque = deque()
+
+    def __len__(self):
+        return len(self._deque)
+
+    def append(self, item):
+        if item:
+            self._deque.append(item)
+
+    def appendleft(self, item):
+        if item:
+            self._deque.appendleft(item)
+
+    def pop(self):
+        return self._deque.pop()
+
+    def popleft(self):
+        return self._deque.popleft()
