3d0e8a6905ecee39a8ac0135c0e0c534b4739e7e
Charles Li <charles9000li@gmail.com>
2020-02-14 00:21:28 -0800

    added command line parser

--- simulate/__main__.py
+++ simulate/__main__.py
@@ -1,2 +1,12 @@
+import argparse
+
+from run import SimulationInformation
+
+
 if __name__ == '__main__':
-    pass
+    parser = argparse.ArgumentParser()
+    parser.add_argument('parameter_file')
+    command_line_args = parser.parse_args()
+
+    simulation_information = SimulationInformation(command_line_args.parameter_file)
+    simulation_information.run()
