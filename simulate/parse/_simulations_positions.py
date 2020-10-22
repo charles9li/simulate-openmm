c48dd3112a08df1ad1482b23aeb6834ebdce2f93
Charles Li <charles9000li@gmail.com>
2020-10-22 09:14:48 -0700

    box vectors now set correctly

--- simulate/parse/_simulations_positions.py
+++ simulate/parse/_simulations_positions.py
@@ -106,9 +106,8 @@ class FileOptions(_PositionOptions):
             t = md.load(self.file, frame=self.frame)
         else:
             t = md.load(self.file, top=self.top, frame=self.frame)
-        topology = t.topology
         simulation.context.setPositions(t.xyz[0])
-        simulation.context.setPeriodicBoxVectors(*topology.getPeriodicBoxVectors())
+        simulation.context.setPeriodicBoxVectors(*t.unitcell_vectors[0])
 
 
 class SubrandomParticlePositions(_PositionOptions):
